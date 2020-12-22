8fc825e47dcc7b3ffabe4aef9ff95542fd599e2d
dakshay <dakshay.devstree@gmail.com>
2020-12-22 21:31:07 +0530

    Added material design dependency in root.

--- app/src/main/java/com/devstree/mediafilepicker/MainActivity.kt
+++ app/src/main/java/com/devstree/mediafilepicker/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.devstree.mediafilepicker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.devstree.mediafilepicker.bottomsheet.BaseBottomSheet
-import com.devstree.mediafilepicker.listener.MediaPickerCallback
+import androidx.appcompat.app.AppCompatActivity
 import com.devstree.mediafilepicker.bottomsheet.BottomSheetFilePicker
 import com.devstree.mediafilepicker.databinding.ActivityMainBinding
+import com.devstree.mediafilepicker.listener.MediaPickerCallback
 import com.devstree.mediafilepicker.model.Media
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -22,23 +21,23 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (v) {
             binding.btnOpenFilePicker -> {
                 val bottomSheetFilePicker = BottomSheetFilePicker()
-//                bottomSheetFilePicker.setMediaListenerCallback(BottomSheetFilePicker.TAKE_ALL, object : MediaPickerCallback {
-//                    override fun onPickedSuccess(media: Media?) {
+                bottomSheetFilePicker.setMediaListenerCallback(BottomSheetFilePicker.TAKE_ALL, object : MediaPickerCallback {
+                    override fun onPickedSuccess(media: Media?) {
 //                        if (media == null) return
 //                        mediaList.add(media)
 //                        mediaAdapter?.setData(mediaList)
 //                        binding.rvMediaItem.scrollToPosition(mediaList.size - 1)
 //                        setUpViewPager()
-//                    }
-//
-//                    override fun onPickedError(error: String?) {
+                    }
+
+                    override fun onPickedError(error: String?) {
 //                        toast(error)
-//                    }
-//
-//                    override fun showProgressBar(enable: Boolean) {
-//                    }
-//                })
-//                bottomSheetFilePicker.show(supportFragmentManager, "take_all")
+                    }
+
+                    override fun showProgressBar(enable: Boolean) {
+                    }
+                })
+                bottomSheetFilePicker.show(supportFragmentManager, "take_all")
             }
         }
     }
